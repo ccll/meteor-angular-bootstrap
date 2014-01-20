5db94271e4116320d37ff477ea8b8e8749d1a806
ccll <cailei@live.com>
2014-01-20 14:00:16 +0800

    Fix package.js

--- package.js
+++ package.js
@@ -1,7 +1,14 @@
 Package.describe({
-  summary: "Bootstrap components written in pure AngularJS. (http://angular-ui.github.io/bootstrap/)"
+    summary: "Bootstrap components written in pure AngularJS. (http://angular-ui.github.io/bootstrap/)"
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('init.js', 'client');
+    api.use('ngMeteor', 'client');
+
+    // angular-bootstrap files.
+    api.add_files('lib/ui-bootstrap-tpls-0.10.0.js', 'client');
+
+    // Client files.
+    api.add_files('init.js', 'client');
 });
+
