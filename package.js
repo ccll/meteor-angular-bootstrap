ba277977a4ebc0b56e62facdfd336375db3ce44c
ccll <cailei@live.com>
2014-03-08 15:07:47 +0800

    'weak' reference is buggy, use manual check instead.

--- package.js
+++ package.js
@@ -2,11 +2,21 @@ Package.describe({
     summary: "Bootstrap components written in pure AngularJS. (http://angular-ui.github.io/bootstrap/)"
 });
 
+function packageExists(pkgname) {
+    var fs = Npm.require('fs');
+    var path = Npm.require('path');
+    var pkgpath = path.join('packages', pkgname);
+    return fs.existsSync(pkgpath);
+}
+
 Package.on_use(function (api, where) {
     api.use('bower', 'client');
 
-    api.use('angularite', 'client', {weak: true});
-    api.use('ngMeteor', 'client', {weak: true});
+    if (packageExists('angularite')) {
+        api.use('angularite', 'client');
+    } else if (packageExists('ngMeteor')) {
+        api.use('ngMeteor', 'client');
+    }
 
     // Install bower components.
     api.add_files('smart.json', 'client');
